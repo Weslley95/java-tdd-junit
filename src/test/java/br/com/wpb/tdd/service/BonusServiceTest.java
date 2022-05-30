6fe54325b2d3012a405801d98f0418def6c4a25f
Weslley Bezerra <weslleybezerra95@gmail.com>
2022-05-30 15:11:11 -0300

    [INCLUDE] Teste para exception do metodo na classe BonusService

--- src/test/java/br/com/wpb/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/wpb/tdd/service/BonusServiceTest.java
@@ -2,6 +2,7 @@ package br.com.wpb.tdd.service;
 
 import br.com.wpb.tdd.modelo.Funcionario;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,27 +14,22 @@ class BonusServiceTest {
     @Test
     void bonusDeveSerZeroParaFuncionarioComSalarioAlto() {
         var service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(
-                new Funcionario(
-                        "Rodrigo",
-                        LocalDate.now(),
-                        new BigDecimal("25000")
-                )
-        );
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(
+                new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
+
+        try {
+            service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+        } catch (IllegalArgumentException e) {
+            assertEquals("Funcionário com salário maior que R$ 10000.00 não recebe bonus", e.getMessage());
+        }
     }
 
     @Test
     void bonusDeveSer10PorcentoDoSalario() {
         var service = new BonusService();
         BigDecimal bonus = service.calcularBonus(
-                new Funcionario(
-                        "Rodrigo",
-                        LocalDate.now(),
-                        new BigDecimal("2500")
-                )
-        );
+                new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
 
         assertEquals(new BigDecimal("250.00"), bonus);
     }
