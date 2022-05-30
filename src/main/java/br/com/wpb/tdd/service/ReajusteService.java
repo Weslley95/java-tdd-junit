d75312fac7db82374cf7bb2cb9c32415accc3aab
Weslley Bezerra <weslleybezerra95@gmail.com>
2022-05-30 12:59:52 -0300

    [REFACTOR] Refatorado codigo, utilizado design patterns strategy

--- src/main/java/br/com/wpb/tdd/service/ReajusteService.java
+++ src/main/java/br/com/wpb/tdd/service/ReajusteService.java
@@ -8,25 +8,7 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        BigDecimal reajuste = funcionario.getSalario().multiply(porcentagemReajuste(desempenho));
-        funcionario.reajustarSalario(reajuste);
-    }
-
-    public BigDecimal porcentagemReajuste(Desempenho desempenho) {
-        BigDecimal porcentagem = new BigDecimal("0.00");
-        switch (desempenho) {
-            case A_DESEJAR:
-                porcentagem = new BigDecimal("0.03");
-                break;
-
-            case BOM:
-                porcentagem = new BigDecimal("0.15");
-                break;
-
-            case OTIMO:
-                porcentagem = new BigDecimal("0.20");
-                break;
-        }
-        return porcentagem;
+        BigDecimal percentual = desempenho.percentualReajuste();
+        funcionario.reajustarSalario(funcionario.getSalario().multiply(percentual));
     }
 }
